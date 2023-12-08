1645c66ee6674bf6c42417b02220025e8c71b233
“Arunthamizh” <arunthamizh.azhagiri@twilightitsolutions.com>
2023-12-08 12:16:55 +0530

    01 to 04th-video sending get request

--- src/App.js
+++ src/App.js
@@ -19,33 +19,33 @@ function App() {
     },
   ];
 
-  // const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-  // const fetchMoviesHandler = () => {
-  //   fetch("https://swapi.dev/api/films")
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       const transformedMovies = data.results.map((movieData) => {
-  //         return {
-  //           id: movieData.episode_id,
-  //           title: movieData.title,
-  //           openingText: movieData.opening_crawl,
-  //           releaseDate: movieData.release_date,
-  //         };
-  //       });
-  //       setMovies(transformedMovies);
-  //     });
-  // };
+  const fetchMoviesHandler = () => {
+    fetch("https://swapi.dev/api/films")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
